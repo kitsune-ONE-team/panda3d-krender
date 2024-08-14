100048d97de14885c5736cbf05c32565f3e14762
Yonnji <yonnji@kitsune.one>
2024-08-14 10:15:15 +0300

    add dummy tests, fix rtti

---
+++ krender_tests.h
@@ -0,0 +1,13 @@
+#include <cxxtest/TestSuite.h>
+
+#include "krender/core/render_pipeline.h"
+#include "pandaNode.h"
+#include "nodePath.h"
+#include <stdio.h>
+
+
+class ChaosTest : public CxxTest::TestSuite {
+public:
+    void test_something(void) {
+    }
+};
